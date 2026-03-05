150d39930645b5ea030b858e7ccbeb1177640690
rlaphoenix <17136956+rlaphoenix@users.noreply.github.com>
2026-03-05 21:27:49 +0000

    Clean up AuthToken

--- auth_token.py
+++ auth_token.py
@@ -3,30 +3,37 @@ import json
 import random
 import time
 
-class AuthToken():
-    def __init__(self, id: str, nonce: str=None, issue_time: int=None):
-        self.id = id
-        if (nonce == None):
-            self.nonce = AuthToken.generate_nonce()
-        else:
-            self.nonce = nonce
 
-        if (issue_time == None):
-            self.issue_time = time.time()
-        else:
-            self.issue_time = issue_time
-    def as_token(self):
-        return base64.b64encode(json.dumps({
+class AuthToken():
+    def __init__(self, id: str, nonce: str | None = None, issue_time: int | None = None):
+        self.id = id
+        self.nonce = nonce
+        self.issue_time = issue_time
+
+        if not self.nonce:
+            self.nonce = AuthToken.generate_nonce()
+
+        if not self.issue_time:
+            self.issue_time = int(time.time())
+
+    def as_token(self) -> str:
+        fields = json.dumps({
             "id": self.id,
             "nonce": self.nonce,
             "issue_time": self.issue_time
-        }).encode('utf8')).decode('utf8')
+        })
+        token = base64.b64encode(fields.encode("utf8")).decode("utf8")
+        return token
+    
+    @staticmethod
     def from_token(token: str):
-        token_payload = json.loads(base64.b64decode(token))
+        payload = json.loads(base64.b64decode(token))
         return AuthToken(
-            token_payload["id"],
-            token_payload["nonce"],
-            token_payload["issue_time"]
+            payload["id"],
+            payload["nonce"],
+            payload["issue_time"]
         )
+
+    @staticmethod
     def generate_nonce():
         return base64.b64encode(random.randbytes(32)).decode()
